d59949b01ddb269a0458fe227e178f4e75ff1731
Francois vd Merwe <vdmfj@yahoo.com>
2020-09-29 09:57:51 +0200

    remove my solution from master

--- src/main/java/nl/sourcelabs/demos/cf/productdata/ProductController.java
+++ src/main/java/nl/sourcelabs/demos/cf/productdata/ProductController.java
@@ -1,6 +1,5 @@
 package nl.sourcelabs.demos.cf.productdata;
 
-import nl.sourcelabs.demos.cf.productdata.fmer.ProductDataServiceFmer;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,13 +11,10 @@ public class ProductController {
 
   private ProductDataService productDataService;
   private ProductDataService2 productDataService2;
-  private ProductDataServiceFmer productDataServiceFmer;
 
-  public ProductController(ProductDataService productDataService, ProductDataService2 productDataService2,
-                           ProductDataServiceFmer productDataServiceFmer) {
+  public ProductController(ProductDataService productDataService, ProductDataService2 productDataService2) {
     this.productDataService = productDataService;
     this.productDataService2 = productDataService2;
-    this.productDataServiceFmer = productDataServiceFmer;
   }
 
   @GetMapping(path = "/productsv1", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -29,11 +25,6 @@ public class ProductController {
   @GetMapping(path = "/productsv2", produces = MediaType.APPLICATION_JSON_VALUE)
   public ProductResponse getProductsV2() {
     return productDataService2.getProducts();
-  }
-
-  @GetMapping(path = "/productsfmer", produces = MediaType.APPLICATION_JSON_VALUE)
-  public ProductResponse getProductsFmer() {
-    return productDataServiceFmer.getProducts();
   }
 
 }
